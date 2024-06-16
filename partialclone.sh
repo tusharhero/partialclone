5466c4f198bb0f6d380f1b961ba4ea2a853f585f
tusharhero <tusharhero@sdf.org>
2024-06-16 10:03:09 +0530

    add initial working script

---
+++ partialclone.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# partialclone.sh - Clone a single directory or file from a Git repository.
+# Copyright (C) 2024 tusharhero
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+giturl=$1
+content=$2
+cachedir="${XDG_CACHE_HOME:-~/.cache}/partialclone"
+
+namespace=$(awk -F '/' '{ print $(NF-1) }' <<< "$giturl")
+repository=$(awk -F '/' '{ print $(NF) }' <<< "$giturl")
+cacherepo="$cachedir/$namespace/$repository"
+echo mkdir "$cacherepo" -p
+
+[[ -d "$cacherepo/.git" ]] &&  git pull "$cacherepo" ||  git clone "$giturl" "$cacherepo"
+
+cp "$cacherepo/$content" . -r
