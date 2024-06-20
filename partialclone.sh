93cf0c34e2ab6b6dc7bfa012372c0b7e617e890a
virinci <revirinci@proton.me>
2024-06-20 18:54:05 +0530

    Handle trailing slash in repository URL

--- partialclone.sh
+++ partialclone.sh
@@ -21,11 +21,17 @@ content=$2
 cachedir="${XDG_CACHE_HOME:-$HOME/.cache}/partialclone"
 cachedir="${PARTIALCLONE_CACHE:-$cachedir}"
 
-namespace=$(echo "$giturl" | awk -F '/' '{ print $(NF-1) }')
 repository=$(echo "$giturl" | awk -F '/' '{ print $(NF) }')
-cacherepo="$cachedir/$namespace/$repository"
-mkdir "$cacherepo" -p
 
+if [ -z "$repository" ]; then
+    # If the URL had a trailing slash.
+    repository=$(echo "$giturl" | awk -F '/' '{ print $(NF-1) }')
+    namespace=$(echo "$giturl" | awk -F '/' '{ print $(NF-2) }')
+else
+    namespace=$(echo "$giturl" | awk -F '/' '{ print $(NF-1) }')
+fi
+
+cacherepo="$cachedir/$namespace/$repository"
 
 if [ -d "$cacherepo/.git" ]
 then
